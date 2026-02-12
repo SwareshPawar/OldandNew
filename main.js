b85bcdcd34856da31229ef3a02b275d81d32e5fa
SwareshPawar <swareshpawar@gmail.com>
2026-02-12 17:40:38 +0530

    Add

--- main.js
+++ main.js
@@ -205,7 +205,7 @@ const CHORD_TYPES = [
         const API_BASE_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
 
             ? 'http://localhost:3001'
-            : 'https://oldandnew.onrender.com'; // Backend server on Render
+            : 'https://oldand-new.vercel.app'; // Backend server on Vercel
 
             console.log('API_BASE_URL:', API_BASE_URL);
         
